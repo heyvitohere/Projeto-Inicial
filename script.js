867ad7da164b4066bb68c3bdb3b4d4222f544daf
heyvitohere <victorsoa9@gmail.com>
2024-03-15 08:23:34 -0300

    variavel nome

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click" , alertar, false );
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("Você clicou no botão!!! " + event);
+    alert("Você clicou no botão!!! " + nome.value);
 
 
 var numero = 6;
 var resultado = numero % 2;
 if(resultado == 0 ){
-    alert ("este número é par");
+    alert ("este número é par: "+numero);
 }
 
 }
